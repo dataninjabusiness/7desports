4241a482b2b2e369f44b15cea9110f421f218a8d
Data Ninja <dataninjabusiness@gmail.com>
2019-07-31 14:49:22 -0400

    setup basic router

--- main.go
+++ main.go
@@ -19,11 +19,25 @@ func main() {
 	router := gin.Default()
 	router.Use(gin.Logger())
 	router.LoadHTMLGlob("templates/*.tmpl.html")
+	router.Static("/static", "static")
 	router.Static("/views", "views")
 
 	router.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.tmpl.html", nil)
 	})
+
+  api := router.Group("/api")
+  {
+    api.GET("", func(c *gin.Context) {
+      c.JSON(http.StatusOK, gin.H {
+        "status": "success",
+        "message": "pong!",
+      })
+    })
+    // api.GET("/users/all", users.GetAllUsers(db))
+    // api.GET("/users/username", users.GetUserByUserName(db))
+    // api.POST("/users/create", users.CreateUser(db))
+  }
 
 	router.Run(":" + port)
 }
